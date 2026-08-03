efde5f555e72121d140b9d0614bd9178ccacaa94
3m0ra <91485919+3m0ra@users.noreply.github.com>
2026-08-03 13:38:07 +0200

    Delete gsandroid directory

--- gsandroid/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-pluginManagement {
-    repositories { google(); mavenCentral(); gradlePluginPortal() }
-}
-dependencyResolutionManagement {
-    repositories { google(); mavenCentral() }
-}
-rootProject.name = "GeoStruct"
-include(":app")
