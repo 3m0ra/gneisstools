25f265499ada957858fe2a68d3d15f9e66b66039
3m0ra <91485919+3m0ra@users.noreply.github.com>
2026-08-04 20:53:00 +0200

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.geostruct.field"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "3.1"
+        versionCode = 5
+        versionName = "3.2"
     }
 
     buildTypes {
